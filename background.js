3629cdae7acd7795e357e5b1353c50f917b4db79
Namaste ElAcechador <elacechador@live.ca>
2018-02-25 23:45:15 -0500

    Use raw images for tumblr

--- background.js
+++ background.js
@@ -16,6 +16,8 @@ function actionUpload(imgUrl, imgSourceUrl) {
 	var uploadUrl = "https://e621.net/post/upload?"
 	var params = new URLSearchParams();
 
+	imgUrl = findBetterImageVersion(imgUrl);
+
 	params.append("url", imgUrl);
 	
 	if (imgSourceUrl !== imgUrl) {
@@ -25,4 +27,29 @@ function actionUpload(imgUrl, imgSourceUrl) {
 	browser.tabs.create({
 		"url": uploadUrl + params.toString(),
 	});
+}
+
+function findBetterImageVersion(imgUrl) {
+	// See : https://e621.net/wiki/show/howto:sites_and_sources
+
+	var urlObj = new URL(imgUrl);
+
+	if (/(\d+\.media|data)\.tumblr\.com/.test(urlObj.host)) {
+		var [, rawHash, fileName] = urlObj.pathname.split("/");
+
+		var regexMatch = /(tumblr(?:_inline)?)_([^\W_]+(?:_r\d+)?)_(\d+|raw)\.([a-z]+)/.exec(fileName);
+		
+		if (regexMatch !== null) {
+			var [, prefix, bucket, res, ext] = regexMatch;
+			res = "raw";
+	
+			fileName = prefix + "_" + bucket + "_" + res + "." + ext;
+	
+			urlObj.pathname = "/" + rawHash + "/" + fileName;
+			urlObj.host = "data.tumblr.com";
+			urlObj.protocol = "http:";
+		}
+	}
+
+	return urlObj.toString();
 }
