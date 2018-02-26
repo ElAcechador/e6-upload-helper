6a8e090a0730efeb7d5afadfe8ad1a8a66664192
Namaste ElAcechador <elacechador@live.ca>
2018-02-25 22:37:29 -0500

    Proof of concept

---
+++ background.js
@@ -0,0 +1,25 @@
+browser.menus.create({
+	id: "direct-upload",
+	title: "Upload to e621.net",
+	type: "normal",
+	contexts: ["image"],
+});
+
+browser.menus.onClicked.addListener(function(info) {
+	if(info.menuItemId === "direct-upload") {
+		// Maybe do something with info.linkUrl?
+		actionUpload(info.srcUrl, info.pageUrl);
+	}
+});
+
+function actionUpload(imgUrl, imgSourceUrl) {
+	var uploadUrl = "https://e621.net/post/upload?"
+	var params = new URLSearchParams();
+
+	params.append("url", imgUrl);
+	params.append("source", imgSourceUrl);
+
+	browser.tabs.create({
+		"url": uploadUrl + params.toString(),
+	});
+}
